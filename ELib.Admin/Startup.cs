9cee0ed9020c99a5e2a030946097bc68111845ee
Snizhana <lavliess@rambler.ru>
2015-10-28 11:25:42 +0200

    add project

---
+++ ELib.Admin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ELib.Admin.Startup))]
+namespace ELib.Admin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
